966a02ddcc64dcb821c117bef4ac6b746da12d62
cagdas_karabulut <ckarabulut@adersystems.com>
2023-04-10 08:25:10 +0300

    .

--- src/main/java/com/karabulut/javapracticeplayground/JavaPracticePlaygroundApplication.java
+++ src/main/java/com/karabulut/javapracticeplayground/JavaPracticePlaygroundApplication.java
@@ -3,10 +3,23 @@ package com.karabulut.javapracticeplayground;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class JavaPracticePlaygroundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaPracticePlaygroundApplication.class, args);
+
+        System.out.println(average(2,1));
     }
+
+    public static double average(int a, int b) {
+        return (a+b)/2;
+    }
+
 }
